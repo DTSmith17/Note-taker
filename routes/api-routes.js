a3d24736614f698ee625034df5e48ddf6eaf85a7
DTSmith17 <153142161+DTSmith17@users.noreply.github.com>
2024-03-24 19:11:04 -0500

    Created a get route in api-routes to read and send notes file

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,41 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const fs = require("fs");
 
+//names of routes in index.js
+//1. get routes for data (res.json) read file and send
+router.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) return res.json(err);
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
 
+//2.post route. get new note and save to db json. res.json(anything)
 
 module.exports = router;
+
+//use as refference
+fs.readFile("./db/reviews.json", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    // Convert string into JSON object
+    const parsedReviews = JSON.parse(data);
+
+    // Add a new review
+    parsedReviews.push(newReview);
+
+    // Write updated reviews back to the file
+    fs.writeFile(
+      "./db/reviews.json",
+      JSON.stringify(parsedReviews, null, 4),
+      (writeErr) =>
+        writeErr
+          ? console.error(writeErr)
+          : console.info("Successfully updated reviews!")
+    );
+  }
+});
