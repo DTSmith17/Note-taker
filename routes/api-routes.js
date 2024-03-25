56cfb4530d996d099d0ac7cd9f5fa02bae3ac5ec
DTSmith17 <153142161+DTSmith17@users.noreply.github.com>
2024-03-24 21:42:30 -0500

    created post route for api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,39 +3,26 @@ const router = express.Router();
 const path = require("path");
 const fs = require("fs");
 
-//names of routes in index.js
-//1. get routes for data (res.json) read file and send
+//get route for data (res.json) read file and send
 router.get("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf8", (err, data) => {
+    console.log(err);
     if (err) return res.json(err);
     const notes = JSON.parse(data);
     res.json(notes);
   });
 });
 
-//2.post route. get new note and save to db json. res.json(anything)
+//get new note and save to db json. res.json(anything)
+router.post("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) return res.json(err);
+    const notes = JSON.parse(data);
+    notes.push(req.body);
+    fs.writeFile("./db/db.json", JSON.stringify(notes, null, 4), (writeErr) =>
+      writeErr ? res.json(writeErr) : res.sendStatus(201)
+    );
+  });
+});
 
 module.exports = router;
-
-//use as refference
-fs.readFile("./db/reviews.json", "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-  } else {
-    // Convert string into JSON object
-    const parsedReviews = JSON.parse(data);
-
-    // Add a new review
-    parsedReviews.push(newReview);
-
-    // Write updated reviews back to the file
-    fs.writeFile(
-      "./db/reviews.json",
-      JSON.stringify(parsedReviews, null, 4),
-      (writeErr) =>
-        writeErr
-          ? console.error(writeErr)
-          : console.info("Successfully updated reviews!")
-    );
-  }
-});
